e2b7bb2a7161c4e5e4026113fa1680c6fc25d1f4
Logan Simonsen <32521559+ENDgameERS@users.noreply.github.com>
2018-02-21 15:20:07 -0600

    completed

--- index.js
+++ index.js
@@ -1,12 +1,53 @@
 const express = require('express');
 const session = require('express-session');
-
+const passport = require('passport');
+const request = require('request');
+const strategy = require(`${__dirname}/strategy.js`);
+const config = require(`${__dirname}/config.js`);
+const { clientID } = config;
 const app = express();
 app.use( session({
   secret: '@nyth!ng y0u w@nT',
   resave: false,
   saveUninitialized: false
 }));
+app.use( passport.initialize() );
+app.use( passport.session() );
+passport.use( strategy );
 
-const port = 3000;
+passport.serializeUser( (user, done) => {
+  console.log('user',user)
+  const { _json } = user;
+  done(null, { clientID: _json.clientID, email: _json.email, name: _json.name, followers: _json.followers_url });
+});
+
+passport.deserializeUser( (obj, done) => {
+  done(null, obj);
+});
+
+app.get( '/login',
+  passport.authenticate('auth0', 
+    { successRedirect: '/followers', failureRedirect: '/login', failureFlash: true, connection: 'github' }
+  )
+);
+
+app.get('/followers', ( req, res, next ) => {
+  console.log(req.user);
+  if ( req.user ) {
+    const FollowersRequest = {
+      url: `https://api.github.com/users/${req.user.nickname}/followers`,
+      headers: {
+        'User-Agent': clientID
+      }
+    };
+
+    request(FollowersRequest, ( error, response, body ) => {
+      res.status(200).send(body);
+    });
+  } else {
+    res.redirect('/login');
+  }
+});
+
+const port = 3001;
 app.listen( port, () => { console.log(`Server listening on port ${port}`); } );
